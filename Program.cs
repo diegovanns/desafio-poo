b2af5008320074d3aed2af51eff9d2d37713ae34
Diego Vanderlei Nunes <diegovanderlei556@hotmail.com>
2024-09-06 21:29:11 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo 1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
